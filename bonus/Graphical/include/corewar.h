3a91b95bca1a5d111940b90324216c8db6e386d2
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-05-16 13:53:00 +0200

    feat: initialized a bonus

---
+++ bonus/Graphical/include/corewar.h
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** B_CPE_200_LIL_2_1_corewar_alexandre_garbe
+** File description:
+** corewar.h
+*/
+
+#ifndef COREWAR_H
+    #define COREWAR_H
+    #include "op.h"
+    #include "../lib/lib.h"
+    #include "macros.h"
+    #include "struct.h"
+    #include "parsing.h"
+    #include "instructions.h"
+    #include "arguments.h"
+    #include "corewar_loop.h"
+    #include <ncurses.h>
+
+void dump_mem(char *mem);
+void init_ncurses(void);
+#endif
